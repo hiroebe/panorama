2fbf6e5b5b16950f34b97d4756bd64c9e803254c
hiroebe <hiro.ebe.albirex12@gmail.com>
2019-07-20 01:52:23 +0900

    add close button to exit from VR

--- js/main.js
+++ js/main.js
@@ -1,57 +1,77 @@
+let camera, scene, renderer, mesh;
+let disposables = [];
+
 window.addEventListener('DOMContentLoaded', () => {
+    registerEvents();
+    initVR();
+});
+
+function registerEvents() {
     document.getElementById('image-input').addEventListener('change', e => {
         const file = e.target.files[0];
         const blobURL = window.URL.createObjectURL(file);
-        loadTexture(blobURL);
-        e.target.remove();
+        showVR(blobURL);
     });
-});
-
-function loadTexture(url) {
-    const loader = new THREE.TextureLoader();
-    loader.load(url, texture => {
-        main(texture);
+    document.getElementById('close-button').addEventListener('click', e => {
+        closeVR();
     });
 }
 
-function main(texture) {
+function initVR() {
     new WebVRPolyfill();
 
     const winWidth = window.innerWidth;
     const winHeight = window.innerHeight;
 
-    const camera = new THREE.PerspectiveCamera(75, winWidth / winHeight, 1, 2000);
+    camera = new THREE.PerspectiveCamera(75, winWidth / winHeight, 1, 2000);
 
-    const scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-    const imgWidth = texture.image.width;
-    const imgHeight = texture.image.height;
-    const r = imgWidth / Math.PI / 2;
-    const theta = Math.atan(imgHeight / 2 / r);
-    const phiStart = 0;
-    const phiLength = Math.PI * 2;
-    const thetaStart = Math.PI / 2 - theta;
-    const thetaLength = theta * 2;
-    const geometry = new THREE.SphereBufferGeometry(500, 60, 40, phiStart, phiLength, thetaStart, thetaLength);
-    geometry.scale(-1, 1, 1);
-    const mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({map: texture}));
-    scene.add(mesh);
-
-    const renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(winWidth, winHeight);
     renderer.vr.enabled = true;
-    document.getElementById('out').appendChild(renderer.domElement);
 
-    document.getElementById('out').appendChild(THREE.WEBVR.createButton(renderer));
+    const container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
+    container.appendChild(THREE.WEBVR.createButton(renderer));
+}
 
-    renderer.setAnimationLoop(tick);
+function showVR(url) {
+    document.getElementById('image-input').style.display = 'none';
+    document.getElementById('container').style.display = 'block';
 
-    let frame = 0;
+    const loader = new THREE.TextureLoader();
+    loader.load(url, texture => {
+        const [thetaStart, thetaLength] = getTheta(texture.image.width, texture.image.height);
+        const geometry = new THREE.SphereBufferGeometry(500, 60, 40, 0, Math.PI * 2, thetaStart, thetaLength);
+        geometry.scale(-1, 1, 1);
 
-    function tick() {
-        frame++;
+        const material = new THREE.MeshBasicMaterial({map: texture});
+        mesh = new THREE.Mesh(geometry, material);
 
-        renderer.render(scene, camera);
+        scene.add(mesh);
+        disposables = [geometry, material, texture];
+        renderer.setAnimationLoop(() => renderer.render(scene, camera));
+    });
+}
+
+function closeVR() {
+    document.getElementById('image-input').style.display = 'inline';
+    document.getElementById('container').style.display = 'none';
+
+    scene.remove(mesh);
+    for (const d of disposables) {
+        d.dispose();
     }
+    renderer.setAnimationLoop(null);
+}
+
+function getTheta(width, height) {
+    const r = width / Math.PI / 2;
+    const theta = Math.atan(height / 2 / r);
+    const thetaStart = Math.PI / 2 - theta;
+    const thetaLength = theta * 2;
+
+    return [thetaStart, thetaLength];
 }
