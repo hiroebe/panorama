c3a62bf9812316e135440f901b2fa68f3922360f
hiroebe <hiro.ebe.albirex12@gmail.com>
2019-07-20 23:03:01 +0900

    update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,22 +1,31 @@
-const CACHE_NAME = 'panorama-caches';
+const CACHE_NAME = 'panorama-caches-v1';
 const FILES_TO_CACHE = [
-    '/index.html',
-    '/dist/main.js',
+    './',
+    './dist/main.js',
 ];
 
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches
-            .open(CACHE_NAME)
+        caches.open(CACHE_NAME)
             .then(cache => cache.addAll(FILES_TO_CACHE))
     );
 });
 
 self.addEventListener('fetch', e => {
     e.respondWith(
-        caches
-            .match(e.request)
-            .then(response => response || fetch(e.request))
+        caches.match(e.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(e.request)
+                    .then(res => {
+                        const resClone = res.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => cache.put(e.request, resClone));
+                        return res;
+                    });
+            })
     );
 });
 
